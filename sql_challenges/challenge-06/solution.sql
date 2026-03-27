df70aedadb00f2a06dfe7939ecb166426f14c385
José Yael Varela García <132051688+Yael-Varela@users.noreply.github.com>
2026-03-26 18:01:07 -0600

    Session-2026-03-26: Comments

--- sql_challenges/challenge-06/solution.sql
+++ sql_challenges/challenge-06/solution.sql
@@ -1,6 +1,8 @@
-//Part 1:
+--José Yael Varela García A01645324
 
-//There is no need for OLD since this is a trigger that fires before inserting each row in the PET_CARE_LOG table.
+--Part 1:
+--There is no need for OLD since this is a trigger that fires before inserting each row in the PET_CARE_LOG table.
+
 CREATE OR REPLACE TRIGGER trg_pet_care_log 
 BEFORE INSERT ON PET_CARE_LOG
 FOR EACH ROW
@@ -11,20 +13,19 @@ BEGIN
  EXCEPTION
     WHEN OTHERS THEN 
     RAISE_APPLICATION_ERROR(
-        -20002,  //Just a code id for the error, we will use the same for all errors since its not needed to be specific.
+        -20002,  --Just a code id for the error, we will use the same for all errors since its not needed to be specific.
         "General error"
     );
 END;
 
 
 
-//Part 2
-
+--Part 2
 CREATE OR REPLACE TRIGGER trg_beforeUpdate
 BEFORE INSERT ON PET_CARE_LOG
 FOR EACH ROW
 BEGIN
-    IF USER != :OLD.UPDATED_BY_USER THEN //Check if it is not the same user, if it is not, it will be activated and display the error message.
+    IF USER != :OLD.UPDATED_BY_USER THEN --Check if it is not the same user, if it is not, it will be activated and display the error message.
         RAISE_APPLICATION_ERROR(
         -20002,
         "It's a different USER"
@@ -41,9 +42,8 @@ END;
 
 
 
-//Part 3
-
-//It's the same process as the previous exercise. However, the conditional statement is where we check if the user is not the correct one (JOEMANAGER). If it is not, the trigger is fired, showing an error, indicating that it's not the appropriate user, and therefore the deletion cannot continue.
+--Part 3
+--It's the same process as the previous exercise. However, the conditional statement is where we check if the user is not the correct one (JOEMANAGER). If it is not, the trigger is fired, showing an error, indicating that it's not the appropriate user, and therefore the deletion cannot continue.
 CREATE OR REPLACE TRIGGER trg_beforeDelete
 BEFORE INSERT ON PET_CARE_LOG
 FOR EACH ROW
